51f289dc90a9331f4a48a9b977039a11ac172648
ryan1002 <ryanmodeste2@gmail.com>
2016-10-28 11:50:47 +0100

    latest version of rail API backend

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,6 +1,6 @@
 class AuthenticationsController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def register
     user = User.new(user_params)
     if user.save
@@ -25,7 +25,7 @@ class AuthenticationsController < ApplicationController
 
   def user_params
     hash = {}
-    hash.merge! params.slice(:username, :first_name, :last_name, :email, :password,  :password_confirmation)
+    hash.merge! params.slice(:first_name, :last_name, :email, :username, :password, :password_confirmation, :role, :rate, :company_name, :borough, :image, :bio)
     hash
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ Event.destroy_all
 
 plumber = User.create!(username: "bobbybro", first_name: "bobby", last_name: "bro", email: "bobby@bobby.com", password: "password", password_confirmation: "password", role: "plumber", rate: 200, company_name: "bobbiesplumbers", borough: "Kingston", image:"http://fillmurray.com/200/200", bio: "Hi, I'm bobby and I'm a plumber")
 
-client = User.create!(username: "kennyken", first_name: "kenny", last_name: "ken", email: "kenny@kenny.com", password: "password", password_confirmation: "password", role: "client", image:"http://fillmurray.com/200/200")
+client = User.create!(username: "kennyken", first_name: "kenny", last_name: "ken", email: "kenny@kenny.com", password: "password", password_confirmation: "password", role: "client", image:"http://fillmurray.com/300/300")
 
 event = Event.create!(title: "Fixing my boiler", startsAt: "Thu Oct 06 2016 00:00:00 GMT+0100 (BST)", endsAt: "Thu Oct 06 2016 00:00:00 GMT+0100 (BST)", client_id: client.id, plumber_id: plumber.id)
